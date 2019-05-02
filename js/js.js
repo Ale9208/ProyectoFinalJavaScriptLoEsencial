5c7f4df6192b73ba55789cd5fd78ca97177a723a
Ale9208 <ron-52@hotmail.com>
2019-05-01 21:54:10 -0600

    Añadiendo estilos al proyecto

---
+++ js/js.js
@@ -0,0 +1,44 @@
+//Objeto JSON estudiantes
+
+var estudiantes= [{
+		"codigo":"001",
+		"nombre":"Julian Jiménez",
+		"nota": 84
+	},{
+		"codigo":"002",
+		"nombre":"Andrea Montero",
+		"nota": 89
+	},{
+		"codigo":"003",
+		"nombre":"Alejandro Martinez",
+		"nota": 74
+	},{
+		"codigo":"004",
+		"nombre":"Marianela Artavia",
+		"nota": 98
+	},{
+		"codigo":"005",
+		"nombre":"Danilo Mena",
+		"nota": 88
+	},{
+		"codigo":"006",
+		"nombre":"Valeria Hernandez",
+		"nota": 79
+	},{
+		"codigo":"007",
+		"nombre":"Andres Moya",
+		"nota": 87
+	},{
+		"codigo":"008",
+		"nombre":"Silvia Robles",
+		"nota": 91
+	},{
+		"codigo":"009",
+		"nombre":"Karla Rodriguez",
+		"nota": 78
+	},{
+		"codigo":"010",
+		"nombre":"Maria Gutierrez",
+		"nota": 82
+	}
+	];
